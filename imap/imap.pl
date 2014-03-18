7ed933f357822f97f6d88a60fead29964d362bb0
David Steele <dsteele@gmail.com>
2014-03-18 19:43:45 -0400

    Delete autobuild-specific files
    Not needed for setup.py.

--- imap/imap.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-#
-    my $imap = new IMAP::Client();
-    $imap->connect(PeerAddr => $server,
-                  ConnectMethod => 'SSL STARTTLS PLAIN',
-                  )
-    or die "Unable to connect to [$server]: ".$imap->error();
