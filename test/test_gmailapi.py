66de5cabb84dd83f41695741321f7e6ac4b8fb82
David Steele <dsteele@gmail.com>
2015-11-20 21:41:14 -0500

    More test cases for the GMailAPI test

--- test/test_gmailapi.py
+++ test/test_gmailapi.py
@@ -13,13 +13,15 @@ base_mail_dict = {
 
 
 @pytest.mark.parametrize("body", (
-        "",
-        "simple body",
-        "body with http://example.com/link",
-    )
-)
+    "",
+    "simple body",
+    "body with http://example.com/link",
+))
 @pytest.mark.parametrize("attach", (False, True))
-def test_gmailapi(web_fxt, tmpfile, attach, body):
+@pytest.mark.parametrize("cc", ([], ["cc@example.com"]))
+@pytest.mark.parametrize("bcc", ([], ["bcc@example.com"]))
+@pytest.mark.parametrize("su", ([], ["subject"]))
+def test_gmailapi(web_fxt, tmpfile, su, bcc, cc, attach, body):
     mail_dict = base_mail_dict.copy()
 
     if body:
@@ -27,6 +29,15 @@ def test_gmailapi(web_fxt, tmpfile, attach, body):
 
     if attach:
         mail_dict['attach'] = [urllib.request.pathname2url(tmpfile)]
+
+    if cc:
+        mail_dict['cc'] = cc
+
+    if bcc:
+        mail_dict['bcc'] = bcc
+
+    if su:
+        mail_dict['su'] = su
 
     gmailapi = gnomegmail.GMailAPI(mail_dict)
     gmailapi.form_message()
