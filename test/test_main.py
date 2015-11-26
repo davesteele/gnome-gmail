e490ffe89916a4133e893ba79832f94250f39859
David Steele <dsteele@gmail.com>
2015-11-26 13:55:02 -0500

    test_main: Add cases for mailto params

--- test/test_main.py
+++ test/test_main.py
@@ -20,11 +20,11 @@ def test_main_quick(default_mailer_fxt, config_fxt, monkeypatch):
     assert gnomegmail.sys.exit.calledwith(0)
 
 
-@pytest.mark.parametrize('mailto', (
-        "mailto:joe@example.com",
-        "mailto:joe@example.com?body=hi",
-    )
-)
+@pytest.mark.parametrize('body', ("", "hi"))
+@pytest.mark.parametrize('attach', ("", "/etc/resolv.conf"))
+@pytest.mark.parametrize('cc', ("", 'cc@example.com'))
+@pytest.mark.parametrize('bcc', ("", 'bcc@example.com'))
+@pytest.mark.parametrize('su', ("", 'subject'))
 @patch(
     'gnomegmail.getGoogleFromAddress',
     Mock(return_value="me@example.com"),
@@ -32,7 +32,16 @@ def test_main_quick(default_mailer_fxt, config_fxt, monkeypatch):
 @patch('gnomegmail.browser', Mock())
 def test_main(default_mailer_fxt, config_fxt, keyring_fxt,
               notify_fxt, web_fxt, oauth_fxt,
-              monkeypatch, mailto):
+              monkeypatch, su, bcc, cc, attach, body):
+
+    args = ['body', 'cc', 'bcc', 'su', 'attach']
+    argvals = locals()
+    queries = ['='.join((x, argvals[x])) for x in args if argvals[x]]
+
+    mailto = "mailto:joe@example.com"
+    if queries:
+        mailto += "?" + '&'.join(queries)
+
     monkeypatch.setattr('gnomegmail.sys.argv', ['prog', mailto])
 
     gnomegmail.main()
