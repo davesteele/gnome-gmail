5d3bee55aa2a2214f1806aba1d99ae9f798328ab
Ville Skyttä <ville.skytta@iki.fi>
2016-01-10 18:34:56 +0200

    Skip browser test also if there's no DISPLAY
    Non-existence of /usr/bin/xdg-settings isn't enough e.g. in Travis.

--- test/test_browser.py
+++ test/test_browser.py
@@ -4,7 +4,8 @@ import pytest
 import os
 
 
-@pytest.mark.skipif(not os.path.exists("/usr/bin/xdg-settings"),
+@pytest.mark.skipif(not os.path.exists("/usr/bin/xdg-settings") or
+                    not os.environ.get("DISPLAY"),
                     reason="Not in a true GUI environment")
 def test_browser():
     browser = gnomegmail.browser()
