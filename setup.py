1d99a4464edc410e9ebfc87ef38806b8731575be
David Steele <dsteele@gmail.com>
2019-04-30 23:13:57 -0400

    v2.7 - redirect OAuth2 to "localhost"
    Use a web server at "localhost" instead of 127.0.0.1, to better
    insure local-only traffic.
    
    Required for the security audit.

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ class my_test(Command):
 
 setup(
     name='gnome-gmail',
-    version='2.6.1',
+    version='2.7',
     description='support for Gmail as the preferred GNOME email application',
     author='David Steele',
     author_email='dsteele@gmail.com',
